269bb3fe46143d5466ee28bb6cef8d79daa08028
Lukasz-Bolanowski <61976130+Lukasz-Bolanowski@users.noreply.github.com>
2026-02-02 15:34:12 +0100

    Add files via upload

--- scrollSlider.js
+++ scrollSlider.js
@@ -1,19 +1,16 @@
-const track = document.querySelector('.slider__track')
-const logos = [...track.children];
-const viewportWidth = track.parentElement.offsetWidth
+const track = document.querySelector('.slider__track');
+const originalLogos = [...track.children];
+const viewportWidth = track.parentElement.offsetWidth;
 
-let trackWidth = track.scrollWidth
-while (trackWidth < viewportWidth * 2) {
-  logos.forEach(logo => {
-    const clone = logo.cloneNode(true)
-    track.appendChild(clone)
-  })
-  trackWidth = track.scrollWidth
+const originalWidth = track.scrollWidth;
+
+while (track.scrollWidth < viewportWidth * 2) {
+  originalLogos.forEach(logo => {
+    track.appendChild(logo.cloneNode(true));
+  });
 }
 
-const singleSetWidth = track.scrollWidth / 2
-const speed = 80
-const duration = singleSetWidth / speed
+const singleSetWidth = originalWidth;
 
-track.style.setProperty('--translate', `${singleSetWidth}px`)
-track.style.setProperty('--duration', `${duration}s`)
+track.style.setProperty('--translate', `${singleSetWidth}px`);
+track.style.setProperty('--duration', `${duration}s`);
